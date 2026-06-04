afbab42b6844293c0e70f9b76797df0623946b12
shuwen <1243889238@qq.com>
2026-06-04 10:53:57 +0800

    feat: 更新样式和组件，优化主题支持和视觉效果

--- packages/layplux/src/layout/root-pane.tsx
+++ packages/layplux/src/layout/root-pane.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: shuwen 1243889238@qq.com
+ * @Date: 2026-05-27 10:24:36
+ * @LastEditors: shuwen 1243889238@qq.com
+ * @LastEditTime: 2026-06-04 10:50:21
+ * @FilePath: /Layplux/packages/layplux/src/layout/root-pane.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineComponent, type PropType } from 'vue';
 import { LayeredManager } from './layered-manager';
 import { GlassOverlay } from './glass-overlay';
@@ -11,7 +19,7 @@ export const RootPane = defineComponent({
   },
   setup(props) {
     return () => (
-      <div class="layplux-root" data-theme="pink">
+      <div class="layplux-root">
         <CornerGlow />
         <LayeredManager skeleton={props.skeleton} />
         <GlassOverlay />
