73e2f505f7ebde8483b3351db8d6d0fce24dd780
shuwen <1243889238@qq.com>
2026-06-10 15:06:53 +0800

    chore: update tsconfig.json to include vite.config.ts in playground and playground-layplux

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
